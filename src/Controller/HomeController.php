ba80f25e4b6bc0d1e994428471ddbbcd40075be1
edwinjo15 <edwinjones.m@gm&il.com>
2025-02-18 14:03:13 +0100

    updated

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Film;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home', methods: ['GET'])]
+    public function index(EntityManagerInterface $entityManager): Response
+    {
+        $latestFilms = $entityManager->getRepository(Film::class)
+            ->findBy([], ['releaseDate' => 'DESC'], 4); // Fetch only the latest 4 films
+
+        return $this->render('home.html.twig', [
+            'latestFilms' => $latestFilms,
+        ]);
+    }
+}
